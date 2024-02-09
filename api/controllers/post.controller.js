3c6cee9fa57a07ea5baeb9b81202965cfe8696c0
Devvrat Gupta <devvrat.gupta11@gmail.com>
2024-02-09 22:28:30 +0530

    complete create post fuctionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,4 +1,5 @@
-import { errorHandler } from "../utils/error"
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js"
 
 export const create = async (req, res,next) => {
     if(!req.user.isAdmin) {
@@ -7,7 +8,11 @@ export const create = async (req, res,next) => {
     if(!req.body.title || !req.body.content){
         return next(errorHandler(400, 'Please provide all required fields'));
     }
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA- Z0-9-]/g,'-');
+    const slug = req.body.title
+    .split(' ')
+    .join('-')
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, '');
     const newPost = new Post({
         ...req.body,
         slug,
@@ -17,6 +22,6 @@ export const create = async (req, res,next) => {
         const savedPost = await newPost.save();
         res.status(201).json(savedPost);
     } catch (error) {
-        
+
     }
 }
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,15 @@ import 'react-quill/dist/quill.snow.css';
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 function CreatePost() {
   const [file,setFile] = useState(null);
   const [imageUploadProgress,setImageUploadProgress] = useState(null);
   const [imageUploadError,setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+  const navigate = useNavigate();
   const handleUpdloadImage = async () => {
     try {
       if(!file){
@@ -46,10 +49,37 @@ function CreatePost() {
       console.log(error);
     }
   }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(!res.ok){
+        setPublishError(data.message);
+        return;
+      }
+      if(data.success===false){
+        setPublishError(data.message);
+        return;
+      }
+      if(res.ok){
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch(error){
+
+    }
+  }
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}> 
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
             type='text'
@@ -57,14 +87,14 @@ function CreatePost() {
             required
             id='title'
             className='flex-1'
-            // onChange={(e) =>
-            //   setFormData({ ...formData, title: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
           />
           <Select
-            // onChange={(e) =>
-            //   setFormData({ ...formData, category: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
           >
             <option value='uncategorized'>Select a category</option>
             <option value='javascript'>JavaScript</option>
@@ -118,11 +148,11 @@ function CreatePost() {
         <Button type='submit' gradientDuoTone='purpleToPink'>
           Publish
         </Button>
-        {/* {publishError && (
+        {publishError && (
           <Alert className='mt-5' color='failure'>
             {publishError}
           </Alert>
-        )} */}
+        )}
       </form>
     </div>
   )
