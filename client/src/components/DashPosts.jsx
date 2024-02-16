9f23f29de9c624f156de7f103a124536586a1948
Devvrat Gupta <devvrat.gupta11@gmail.com>
2024-02-16 20:23:15 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -17,15 +17,34 @@ function DashPosts() {
                 const data = await res.json();
                 if(res.ok){
                     setUserPosts(data.posts);
+                    if(data.posts.length<9){
+                        setShowMore(false);
+                    }
                 }
             } catch(error) {
-
+                console.log(error);
             }
         };
         if(currentUser.isAdmin){
             fetchPosts();
         }
     },[currentUser._id])
+    const handleShowMore = async() => {
+        const startIndex = userPosts.length;
+        try {
+            const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=
+            ${startIndex}`);
+            const data = await res.json();
+            if(res.ok){
+                setUserPosts((prev) => [...prev, ...data.posts]);
+                if(data.posts.lenght<9){
+                    setShowMore(false);
+                }
+            }
+        } catch(error) {
+            consonle.log(error);
+        }
+    }
   return (
      <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -90,7 +109,7 @@ function DashPosts() {
           </Table>
           {showMore && (
             <button
-            //   onClick={handleShowMore}
+              onClick={handleShowMore}
               className='w-full text-teal-500 self-center text-sm py-7'
             >
               Show more
