195fa4a8287a9c6dfc22d2bd3324a5f11ba51788
Devvrat Gupta <devvrat.gupta11@gmail.com>
2024-02-22 22:05:06 +0530

    add recect article section

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -35,7 +35,21 @@ function PostPage() {
             }
         }    
         fetchPost();
-    },[postSlug])
+    },[postSlug]);
+    useEffect(() => {
+      try {
+        const fetchRecentPosts = async() => {
+          const res = await fetch(`/api/post/getPosts?limit=4`);
+          const data = await res.json();
+          if(res.ok){
+            setRecentPosts(data.posts);
+          }
+        }
+        fetchRecentPosts();
+      } catch(error) {
+        console.log(error.message);
+      }
+    })
     if(loading)
         return (
             <div className='flex justify-center items-center min-h-screen'>
@@ -74,7 +88,6 @@ function PostPage() {
         <CallToAction />
       </div>
       <CommentSection postId={post._id} />
-
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
