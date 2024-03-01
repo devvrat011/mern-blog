5338c0a256b3c5452932cc0ccc7542bf03909614
Devvrat Gupta <devvrat.gupta11@gmail.com>
2024-03-01 12:43:03 +0530

    bug fixed again

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
@@ -20,13 +19,12 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
@@ -34,13 +32,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/post',postRoutes);
-app.use('/api/comment',commentRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'client' , 'dist' , 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
